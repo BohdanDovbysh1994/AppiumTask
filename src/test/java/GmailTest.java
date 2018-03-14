e632aecd7684a980c643ef91c0abe5fd96ea54d0
dovbysh.bohdan@gmail.com <FuckOff666666>
2018-03-14 22:31:05 +0200

    refactor

--- src/test/java/GmailTest.java
+++ src/test/java/GmailTest.java
@@ -1,7 +1,4 @@
 import driver.DriverObject;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -9,25 +6,12 @@ import org.testng.annotations.Test;
 import pageobjects.GmailPage;
 import pageobjects.LoginPage;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
 public class GmailTest {
-//    private AppiumDriver driver;
     private LoginPage loginPage;
     private GmailPage gmailPage;
 
     @BeforeMethod
-    public void setUp() throws MalformedURLException {
-//        DesiredCapabilities capability = new DesiredCapabilities();
-//        capability.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-//        capability.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.0");
-//        capability.setCapability(MobileCapabilityType.DEVICE_NAME, "Samsung Galaxy S6");
-//        capability.setCapability(MobileCapabilityType.BROWSER_NAME, "Browser");
-//        URL url = new URL("http://127.0.0.1:4723/wd/hub");
-//        driver = new AppiumDriver(url, capability);
-//        driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+    public void setUp() {
         loginPage = new LoginPage(DriverObject.getDriver());
         gmailPage = new GmailPage(DriverObject.getDriver());
     }
